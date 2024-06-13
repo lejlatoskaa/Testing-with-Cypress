061d6f94b7d21eeb51229aa75d0ca8c166cd6c6d
lejlatoskaa <lt29325@seeu.edu.mk>
2024-06-13 18:37:53 +0200

    Initial commit: Adding existing Cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-    baseUrl: 'https://www.inspire11.com/',
-    experimentalSessionAndOrigin: true,
-    // experimentalModifyObstructiveThirdPartyCode: true,
-    experimentalOriginDependencies: true,
-    chromeWebSecurity: false,
+  baseUrl: "https://www.inspire11.com/",
+  experimentalSessionAndOrigin: true,
+
+  // experimentalModifyObstructiveThirdPartyCode: true,
+  experimentalOriginDependencies: true,
+
+  chromeWebSecurity: false,
+
+  setupNodeEvents(on, config) {
+    // implement node event listeners here
+  },
+
+  e2e: {
     setupNodeEvents(on, config) {
-        // implement node event listeners here
-    }
+      // implement node event listeners here
+    },
+  },
 });
